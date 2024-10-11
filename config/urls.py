862cfdcbdb2947a72d8b917fb39c5be67e17d29b
MohammadAminSohele <mohammadaminsohele@gmail.com>
2024-10-11 21:18:08 +0330

    add Test home page view

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from Articles.views import home_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_page,name='home_page'),
     path('Article/',include('Articles.urls')),
 ]
