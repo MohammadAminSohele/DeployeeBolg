18937419fc91555af08edf6c8fe23d92fb49ce7b
MohammadAminSohele <mohammadaminsohele@gmail.com>
2024-10-14 22:54:45 +0330

    Create Articles Model

--- Articles/models.py
+++ Articles/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+class Article(models.Model):
+    STATUS_CHOICES=(
+        ('p','پابلیش'),
+        ('d','پیش نویس')
+    )
+    title=models.CharField(max_length=100,verbose_name='عنوان')
+    slug=models.SlugField(max_length=50,verbose_name='عنوان در url')
+    description=models.TextField(verbose_name='توضیحات')
+    images=models.ImageField(upload_to='images',verbose_name='عکس')
+    published=models.DateTimeField(default=timezone.now,verbose_name='تاریخ انتشار')
+    created=models.DateTimeField(auto_now_add=True,verbose_name='تاریخ ساخت')
+    updated=models.DateTimeField(auto_now=True,verbose_name='تاریخ ویرایش')
+    status=models.CharField(max_length=1,choices=STATUS_CHOICES,verbose_name='حالت')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name='مقاله'
+        verbose_name_plural='مقالات'
