3c1ef40117c8421ca606558e137d3fd77309fdf4
MohammadAminSohele <mohammadaminsohele@gmail.com>
2024-10-16 12:58:42 +0330

    Add Slider

--- Articles/views.py
+++ Articles/views.py
@@ -6,6 +6,7 @@ from .models import Article
 
 def home_page(request):
     context={
-        'Article':Article.objects.filter(status='p').order_by('created')
+        'Article':Article.objects.filter(status='p').order_by('created'),
+        'Last_slider':Article.objects.order_by('-created').all()[:1]
     }
     return render(request,'home_page.html',context)
