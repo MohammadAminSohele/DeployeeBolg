815db79678da3725338bfd5d1739b9cc58da3e6c
MohammadAminSohele <mohammadaminsohele@gmail.com>
2024-10-18 07:56:31 +0330

    Show date in Temporaroy Mode Should Progress

--- extentions/utils.py
+++ extentions/utils.py
@@ -1,4 +1,15 @@
+from .import jalali 
 
+from django.utils import timezone
 
-def JalaliConverter(data):
-    return data
+def JalaliConverter(time):
+    jmonth=['فروردین','اردیبهشت','خرداد','تیر','مرداد','شهریور','مهر','آبان','آذر','دی','بهمن','اسفند']
+    time=timezone.localtime(time)
+    time_to_str="{},{},{}".format(time.year,time.month,time.day)
+    time_to_tuple=jalali.Gregorian(time_to_str).persian_tuple()
+    time_to_list=list(time_to_tuple)
+    for index,month in enumerate(jmonth):
+        if time_to_list[1]==index+1:
+            time_to_list[1]=month
+            break
+    return time_to_list
