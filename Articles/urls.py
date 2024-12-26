af74c452b160111d0b89732272f7c5415d97319f
MohammadAminSohele <mohammadaminsohele@gmail.com>
2024-12-26 07:18:14 +0330

    change function method home page to Article list , list view

--- Articles/urls.py
+++ Articles/urls.py
@@ -5,8 +5,8 @@ from .import views
 app_name='Article'
 
 urlpatterns = [
-    path('',views.home_page,name='home_page'),
-    path('page/<int:page>',views.home_page,name='home_page'),
+    path('',views.ArticleList.as_view(),name='home_page'),
+    path('page/<int:page>',views.ArticleList.as_view(),name='home_page'),
     path('Article/<slug:slug>',views.Article_detail,name='Article_detail'),
     path('Article/catagory/<slug:slug>',views.Show_articles_by_Catagory,name='Show_articles_by_Catagory'),
     path('Article/catagory/<slug:slug>/page/<int:page>',views.Show_articles_by_Catagory,name='Show_articles_by_Catagory'),
--- Articles/views.py
+++ Articles/views.py
@@ -1,20 +1,36 @@
 from django.shortcuts import render,get_object_or_404
 
 from django.core.paginator import Paginator
+from django.views.generic import ListView
 
 from .models import Article,Catagory,Site_settings
 
 # Create your views here.
 
-def home_page(request,page=1):
-    Articles_list=Article.objects.published()
-    paginator=Paginator(Articles_list,3)
-    articles=paginator.get_page(page)
-    context={
-        'Article':articles,
-        'Last_slider':Article.objects.all()[:1]
-    }
-    return render(request,'home_page.html',context)
+# def home_page(request,page=1):
+#     Articles_list=Article.objects.published()
+#     paginator=Paginator(Articles_list,3)
+#     articles=paginator.get_page(page)
+#     context={
+#         'Article':articles,
+#         'Last_slider':Article.objects.all()[:1]
+#     }
+#     return render(request,'home_page.html',context)
+
+class ArticleList(ListView):
+    paginate_by=3
+    template_name='home_page.html'
+    def get_queryset(self):
+        global articles
+        articles=Article.objects.published()
+        return articles
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Article"] = articles
+        context["Last_slider"] = Article.objects.all()[:1]
+        return context
+    
+    
 
 def Article_detail(request,slug):
     context={
