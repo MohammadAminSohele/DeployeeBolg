74495c8fde74c65425ceac6031dcc99f35066977
MohammadAminSohele <mohammadaminsohele@gmail.com>
2025-01-08 07:07:23 +0330

    Create Account App

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('Articles.urls')),
+    path('',include('account.urls')),
     path('admin/', admin.site.urls),
 ]
 
