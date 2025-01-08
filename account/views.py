51f2ecfae849d3c28f6388077329287602cc3d17
MohammadAminSohele <mohammadaminsohele@gmail.com>
2025-01-08 07:32:04 +0330

    Sample Login concept and configuration

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.views.generic import ListView
+
+from Articles.models import Article
+
 # Create your views here.
+
+@login_required
+def home(request):
+    return render(request,'registration/home.html')
+
+class Article_list(LoginRequiredMixin,ListView):
+    queryset=Article.objects.all()
+    template_name='registration/home.html'
